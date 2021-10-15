6443e67063dbf22b391dc3e651f0246e53d46540
Daniel JS <daniel@mydeveloperjourney.com>
2021-10-15 10:37:18 -0500

    wip: adds additional social icons

--- src/components/social-icons.js
+++ src/components/social-icons.js
@@ -4,13 +4,18 @@ function SocialIcons() {
     return (
         <ul className="social-icons">
             <li>
-                <a href="https://linkedin.com/in/daniel-j-scott">
+                <a href="https://linkedin.com/in/daniel-j-scott" target="_blank" rel="noopener noreferrer">
                     <i className="fab fa-linkedin-in"></i>
                 </a>
             </li>
             <li>
-                <a href="https://github.com/myDeveloperJourney">
+                <a href="https://github.com/myDeveloperJourney" target="_blank" rel="noopener noreferrer">
                     <i className="fab fa-github"></i>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.youtube.com/channel/UCmPSUtVgth9zyXpzOM5lNFQ" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-youtube"></i>
                 </a>
             </li>
         </ul>
