b4bef3995191efe2ce1cefd295da32d9c128a9ac
myDeveloperJourney <daniel@mydeveloperjourney.com>
2021-10-16 19:59:14 -0500

    wip: changes background color

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         name: `DanielJS`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#FBE693`,
+        background_color: `#fff`,
         theme_color: `#FBE693`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`,
