f237082715e6b7c5cb7dcedcd5c06f279f9ab3f5
Daniel JS <daniel@mydeveloperjourney.com>
2022-04-02 16:01:47 -0500

    wip: adds current code for now

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -4,16 +4,35 @@ import Img from 'gatsby-image';
 import SEO from '../components/seo';
 import { graphql } from 'gatsby';
 
+const TagButton = ({ handleClick, tag, tagSelected }) => {
+  return (
+    <button
+      className={`btn btn-dark${!(tag === tagSelected) ? '' : ' border-warning text-warning'}`}
+      onClick={() => handleClick(tag)}>
+      {tag}
+    </button>
+  )
+}
+
 class ArticlePost extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      NoOfPost: 6
+      NoOfPost: 6,
+      tags: [],
+      tagSelected: null,
+      filteredArticles: []
     };
-    this.handleScroll = this.handleScroll.bind(this);
   }
 
   componentDidMount() {
+    const tags = this.props
+      .data
+      .data
+      .allContentfulIdentifier
+      .edges.map(({ node }) => node.identifier);
+
+    this.setState({ tags });
     window.addEventListener('scroll', this.handleScroll);
   }
 
@@ -30,38 +49,60 @@ class ArticlePost extends React.Component {
         NoOfPost: count
       });
     }
-  };
+  }
+
+  handleClick = (tag) => {
+    this.setState({ tagSelected: tag });
+    const filteredArticles = this.props
+      .data.data.allContentfulArticle.edges.filter(({ node }) => node.tags.find(t => t.identifier === tag));
+    this.setState({ filteredArticles })
+  }
+
 
   render() {
 
-    const { data } = this.props;
-    const { NoOfPost } = this.state;
-
+    const { NoOfPost, tagSelected, filteredArticles } = this.state;
+    const { data: { allContentfulArticle } } = this.props.data;
+    const articles = filteredArticles.length ? filteredArticles : allContentfulArticle;
     return (
       <React.Fragment>
+        <div className="d-flex justify-content-around flex-wrap mb-5">
+          {
+            this.state.tags.map((tag, i) =>
+              <TagButton
+                key={i}
+                handleClick={this.handleClick}
+                tag={tag}
+                tagSelected={tagSelected}
+              />
+            )
+          }
+        </div>
         <ul className="article-list" onScroll={this.onScrollEvent}>
-          {data.data.allContentfulArticle.edges.slice(0, NoOfPost).map(items => (
-            <li>
-              <div className="post-item template-square columned">
-                <div className="post-thumbnail">
-                  <Img sizes={items.node.featureImage.fluid} />
-                  <div className="post-date">
-                    <i className="fas fa-calendar-alt"></i>
-                    {items.node.publicData}
+          {articles.edges.slice(0, NoOfPost).map(({ node }) => {
+            return (
+              <li key={node.id}>
+                <div className="post-item template-square columned">
+                  <div className="post-thumbnail">
+                    <Img sizes={node.featureImage.fluid} />
+                    <div className="post-date">
+                      <i className="fas fa-calendar-alt"></i>
+                      {node.publicData}
+                    </div>
+                  </div>
+                  <div className="post-details">
+                    <h2 className="post-title"><Link to={`/${node.slug}`}>{node.title}</Link></h2>
+                    <div className="author">
+                      <Img sizes={node.author.photo.fluid} />
+                      <strong className="name">{node.author.name}</strong>
+                    </div>
+                    <p><small className="text-muted">Time to read: {node.body.childMarkdownRemark.timeToRead} mins</small></p>
+                    <p>{node.body.childMarkdownRemark.excerpt}</p>
                   </div>
                 </div>
-                <div className="post-details">
-                  <h2 className="post-title"><Link to={`/${items.node.slug}`}>{items.node.title}</Link></h2>
-                  <div className="author">
-                    <Img sizes={items.node.author.photo.fluid} />
-                    <strong className="name">{items.node.author.name}</strong>
-                  </div>
-                  <p><small className="text-muted">Time to read: {items.node.body.childMarkdownRemark.timeToRead} mins</small></p>
-                  <p>{items.node.body.childMarkdownRemark.excerpt}</p>
-                </div>
-              </div>
-            </li>
-          ))}
+              </li>
+            )
+          })}
         </ul>
       </React.Fragment>
     );
@@ -85,12 +126,22 @@ export default Articles;
 
 export const query = graphql`
   query ArticleQuery {
+    allContentfulIdentifier {
+      edges {
+        node {
+          identifier
+        }
+      }
+    }
     allContentfulArticle {
         edges {
           node {
             id
             title
             slug
+            tags {
+              identifier
+            }
             publicData(formatString: "MMMM D, YYYY")
             author {
               name
