9e4f30f7e5906ec8ee0e2f0315ecccff842b5e5c
Daniel JS <daniel@mydeveloperjourney.com>
2022-04-04 10:55:51 -0500

    wip: adds articles to front page

---
+++ src/components/articlePost.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react';
+import { Link } from 'gatsby';
+import Img from 'gatsby-image';
+
+
+function ArticlePost({ allContentfulArticle, numPosts }) {
+    console.log({ allContentfulArticle, numPosts })
+    return (
+        <Fragment>
+            <ul className="article-list">
+                {allContentfulArticle.edges.slice(0, numPosts).map(({ node }) => (
+                    <li>
+                        <div className="post-item template-square columned">
+                            <div className="post-thumbnail">
+                                <Img sizes={node.featureImage.fluid} />
+                                <div className="post-date">
+                                    <i className="fas fa-calendar-alt"></i>
+                                    {node.publicData}
+                                </div>
+                            </div>
+                            <div className="post-details">
+                                <h2 className="post-title"><Link to={`/${node.slug}`}>{node.title}</Link></h2>
+                                <div className="author">
+                                    <Img sizes={node.author.photo.fluid} />
+                                    <strong className="name">{node.author.name}</strong>
+                                </div>
+                                <p><small className="text-muted">Time to read: {node.body.childMarkdownRemark.timeToRead} mins</small></p>
+                                <p>{node.body.childMarkdownRemark.excerpt}</p>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </Fragment>
+    );
+}
+
+export default ArticlePost;
