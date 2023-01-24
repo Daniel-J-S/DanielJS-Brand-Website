6a747ed9931dd45efc07544e40928351b5f95212
Daniel JS <daniel@mydeveloperjourney.com>
2023-01-24 14:03:55 -0600

    wip: repaires gatsby image

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { Link } from 'gatsby';
 import Seo from '../components/seo';
 import { graphql } from 'gatsby';
 
-class ArticlePost extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      NoOfPost: 6
-    };
-    this.handleScroll = this.handleScroll.bind(this);
+class ArticlePost extends Component {
+  state = {
+    noOfPost: 6
   }
 
   componentDidMount() {
@@ -21,59 +18,67 @@ class ArticlePost extends React.Component {
   }
 
   handleScroll = () => {
-    var lastScrollY = window.pageYOffset + 1100;
+    const lastScrollY = window.pageYOffset + 1100;
 
     if (lastScrollY > window.outerHeight) {
-      var count = this.state.NoOfPost + 3;
+      const count = this.state.noOfPost + 3;
       this.setState({
-        NoOfPost: count
+        noOfPost: count
       });
     }
-  };
+  }
 
   render() {
 
-    const { data } = this.props;
-    const { NoOfPost } = this.state;
+    const { allContentfulArticle } = this.props;
+    const { noOfPost } = this.state;
 
     return (
-      <React.Fragment>
+      <Fragment>
         <ul className="article-list" onScroll={this.onScrollEvent}>
-          {data.data.allContentfulArticle.edges.slice(0, NoOfPost).map(items => (
-            <li>
-              <div className="post-item template-square columned">
-                <div className="post-thumbnail">
-                  <div className="post-date">
-                    <i className="fas fa-calendar-alt"></i>
-                    {items.node.publicData}
+          {allContentfulArticle.edges.slice(0, noOfPost).map(({ node })=> {
+            
+            const featureImage = getImage(node.featureImage);
+            const authorImage = getImage(node.author.photo);
+            
+            return (
+              <li>
+                <div className="post-item template-square columned">
+                  <div className="post-thumbnail">
+                  <GatsbyImage image={featureImage} alt={node.title} />
+                    <div className="post-date">
+                      <i className="fas fa-calendar-alt"></i>
+                      {node.publicData}
+                    </div>
+                  </div>
+                  <div className="post-details">
+                    <h2 className="post-title"><Link to={`/${node.slug}`}>{node.title}</Link></h2>
+                    <div className="author">
+                      <GatsbyImage image={authorImage} alt={node.author.name} />
+                      <strong className="name">{node.author.name}</strong>
+                    </div>
+                    <p><small className="text-muted">Time to read: {node.body.childMarkdownRemark.timeToRead} mins</small></p>
+                    <p>{node.body.childMarkdownRemark.excerpt}</p>
                   </div>
                 </div>
-                <div className="post-details">
-                  <h2 className="post-title"><Link to={`/${items.node.slug}`}>{items.node.title}</Link></h2>
-                  <div className="author">
-                    <strong className="name">{items.node.author.name}</strong>
-                  </div>
-                  <p><small className="text-muted">Time to read: {items.node.body.childMarkdownRemark.timeToRead} mins</small></p>
-                  <p>{items.node.body.childMarkdownRemark.excerpt}</p>
-                </div>
-              </div>
-            </li>
-          ))}
+              </li>
+            )
+          })}
         </ul>
-      </React.Fragment>
+      </Fragment>
     );
   }
 }
 
-const Articles = data => (
+const Articles = ({ data: {allContentfulArticle}, location }) => (
   <>
     <Seo
       title="Articles"
       keywords={[`gatsby`, `articles`, `react`]}
-      location={data.location}
+      location={location}
     />
     <div className="container article-page Page">
-      <ArticlePost data={data} />
+      <ArticlePost allContentfulArticle={allContentfulArticle} />
     </div>
   </>
 )
@@ -83,26 +88,30 @@ export default Articles;
 export const query = graphql`
   query ArticleQuery {
     allContentfulArticle {
-        edges {
-          node {
-            id
-            title
-            slug
-            publicData(formatString: "MMMM D, YYYY")
-            author {
-              name
-            }
-            body {
-              childMarkdownRemark {
-                excerpt(pruneLength: 250)
-                timeToRead  
-              }
-            }
+    edges {
+      node {
+        id
+        title
+        slug
+        featureImage {
+          gatsbyImageData(width: 900)
+        }
+        author {
+          name
+          photo {
+            gatsbyImageData(width: 500)
+          }
+        }
+        body {
+          childMarkdownRemark {
+            excerpt(pruneLength: 250)
+            timeToRead
           }
         }
       }
+    }
   }
-`;
+}`;
 
 
 
