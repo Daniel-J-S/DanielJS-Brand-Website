700e42af738f4850c858e6801ccfa9637ea96a1c
Daniel JS <daniel@mydeveloperjourney.com>
2023-01-24 14:11:05 -0600

    wip: repairs resume photos

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Seo from '../components/seo';
 
-function Resume({ data }) {
+function Resume({ data: { 
+    contentfulResume: {
+        updatedAt, 
+        resumePdf, 
+        author, 
+        title, 
+        body, 
+    }} }) {
+    const image = getImage(author.photo)
     return (
         <>
             <Seo title="My Resume" description="I am a self-motivated, hard-working, full-stack developer and educator with a strong passion for innovation and technology." />
             <div style={{ maxWidth: '850px' }} className="Page container">
-                <h2 style={{ display: 'flex', alignItems: 'center' }}>{data.contentfulResume.title}</h2>
+                <h2 
+                    style={{ display: 'flex', alignItems: 'center' }}>
+                    {title}
+                    <GatsbyImage 
+                        style={{ height: '4rem', width: '4rem' }} 
+                        className="rounded-circle img-fluid ms-2"
+                        image={image}
+                        alt={author.name}
+                    />
+                </h2>
                 <p>
-                    <small><strong>Last Updated: {data.contentfulResume.updatedAt}</strong></small>
+                    <small><strong>Last Updated: {updatedAt}</strong></small>
                 </p>
                 <p>
-                    <small><strong>You can also download a copy <a href={`https:${data.contentfulResume.resumePdf.file.url}`} download>here</a></strong></small>
+                    <small><strong>You can also download a copy <a href={`https:${resumePdf.file.url}`} download>here</a></strong></small>
                 </p>
                 <hr />
                 <div dangerouslySetInnerHTML={{
-                    __html: data.contentfulResume.body.childMarkdownRemark.html
+                    __html: body.childMarkdownRemark.html
                 }} />
             </div>
         </>
@@ -28,6 +46,8 @@ export default Resume;
 export const query = graphql`
     query Resume {
         contentfulResume(title: {eq: "My Resume"}) {
+        title
+        updatedAt(formatString: "MM/DD/YYYY")
         body {
             childMarkdownRemark {
                 html
@@ -35,14 +55,15 @@ export const query = graphql`
         }
         author {
             name
-        }
-        title
-        resumePdf {
-            file {
-            url
+            photo {
+                gatsbyImageData(width: 500)
             }
         }
-        updatedAt(formatString: "MM/DD/YYYY")
+        resumePdf {
+                file {
+                    url
+                }
+            }
         }
     }  
 `;
